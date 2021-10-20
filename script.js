4ff853392403dc2d47d2699c397ef4bb8cad12f4
Isaac J <duckhunterjr@gmail.com>
2021-10-19 21:30:21 -0700

    add basic outline of site w/CSS

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log(dayjs());
 const currentDateContainer = document.querySelector(".current-date");
-const shotDateContainer = document.querySelector(".shot-date");
+const shotDateContainer = document.querySelector(".last-shot-date");
 
-const currentDateObject = dayjs().format('dddd, MMM D YYYY');
-const shotDateObject = dayjs().subtract(14,'day').format('M / DD / YYYY');
+const currentDateObject = dayjs().format("dddd, MMM D YYYY");
+const shotDateObject = dayjs().subtract(14, "day").format("M / DD / YYYY");
 
 currentDateContainer.innerText = currentDateObject;
 shotDateContainer.innerText = shotDateObject;
